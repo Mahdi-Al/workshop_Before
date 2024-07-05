0811e44514937e6890711f07f1a386d293847261
Mahdi Alikhani2005 <mahdialikhaniapr2005@gmail.com>
2024-07-05 19:39:44 +0330

    Push

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const input = document.querySelector("input");
 const btnSubmit = document.querySelector("#submit-btn");
 const wrapper = document.querySelector(".wrapper");
-let gifCount = 10;
+let gifCount = 16;
 const apiKey = "BNesU8TGHMDTWmw8OC7skeluVIuvLtRd";
 const generateGif = () => {
   //  display loader until gif load
@@ -19,19 +19,42 @@ const generateGif = () => {
       console.log(info);
       let gifs = info.data.forEach((gif) => {
         let container = document.createElement("section");
-        container.classList.add("container");
+        container.classList.add(
+          "container",
+          "w-full",
+          "md:w-1/2",
+          "lg:w-1/4",
+          "px-4",
+          "py-6"
+        ); // add responsive classes
         let iframe = document.createElement("img");
         iframe.setAttribute("src", gif.images.fixed_height_downsampled.url);
-
         iframe.onload = () => {
-          //  if iframes loaded correctly ...
+          //  if iframes loaded correctly...
           gifCount--;
           if (gifCount == 0) {
             loader.style.display = "none";
-            wrapper.style.display = "grid";
+            wrapper.style.display = "flex";
+            wrapper.classList.add("flex", "flex-wrap", "justify-center"); // add flex, flex-wrap, and justify-center classes
           }
         };
-        container.append(iframe);
+        container.innerHTML += `<div class="w-full rounded-md border grid justify-items-centre ">
+        
+  <img
+    src="${gif.images.fixed_height_downsampled.url}"
+    alt="Laptop"
+    class="h-[200px] w-full rounded-md object-cover "
+  />
+
+    <button
+      type="button"
+      class="mt-4 rounded-sm bg-black px-2.5 py-1 text-[10px] font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black justify-self-center"
+    >
+      more...
+    </button>
+  </div>
+</div>
+`;
         wrapper.append(container);
       });
     })
@@ -40,6 +63,3 @@ const generateGif = () => {
 
 btnSubmit.addEventListener("click", generateGif);
 console.log(btnSubmit);
-// `https://api.giphy.com/v1/gifs/search?api-key=${apiKey}&q=${
-input.value;
-// }&limit=${20}&offset=0&rating=g&lang=en`;
